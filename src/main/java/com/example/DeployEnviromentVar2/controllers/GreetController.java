8531006fd3c3d6da33a90f6aac9e111a48b4818e
LuigiMottola <sputnick427@gmail.com>
2024-04-23 09:55:37 +0200

    Termine esercizio

---
+++ src/main/java/com/example/DeployEnviromentVar2/controllers/GreetController.java
@@ -0,0 +1,21 @@
+package com.example.DeployEnviromentVar2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetEnviroment")
+public class GreetController {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping
+    public ResponseEntity<String> greetEnviroment(){
+        return ResponseEntity.ok(environment.getProperty("welcomeMsg"));
+    }
+}
